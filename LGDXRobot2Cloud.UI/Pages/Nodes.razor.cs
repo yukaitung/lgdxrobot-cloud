fa52bb821b8b77c061d71dc46bb7c6295645b660
Kai Tung Yu <sam@bristolgram.uk>
2024-04-11 21:25:32 +0100

    search and paging

--- LGDXRobot2Cloud.UI/Pages/Nodes.razor.cs
+++ LGDXRobot2Cloud.UI/Pages/Nodes.razor.cs
@@ -2,6 +2,7 @@ using LGDXRobot2Cloud.Shared.Entities;
 using LGDXRobot2Cloud.Shared.Services;
 using LGDXRobot2Cloud.UI.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace LGDXRobot2Cloud.UI.Pages
 {
@@ -9,10 +10,37 @@ namespace LGDXRobot2Cloud.UI.Pages
   {
     [Inject]
     public required INodeService NodeService { get; set; }
+    private List<Node>? NodesList { get; set; } = default!;
+    private PaginationMetadata? PaginationMetadata { get; set; } = default!;
+    private int DataEntriesNumber { get; set; } = 10;
+    private string DataSearch { get; set; } = string.Empty;
 
-    public List<Node>? NodesList { get; set; } = default!;
+    private async Task ChangePage(MouseEventArgs e, int pageNum)
+    {
+      if (pageNum > PaginationMetadata?.PageCount || pageNum < 1)
+        return;
+      var data = await NodeService.GetNodesAsync(DataSearch, pageNum, DataEntriesNumber);
+      NodesList = data.Item1?.ToList();
+      PaginationMetadata = data.Item2;
+    }
 
-    public PaginationMetadata? PaginationMetadata { get; set; }
+    private async Task ChangeShowingEntries()
+    {
+      if (DataEntriesNumber > 100)
+        DataEntriesNumber = 100;
+      else if (DataEntriesNumber < 1)
+        DataEntriesNumber = 1;
+      var data = await NodeService.GetNodesAsync(DataSearch, 1, DataEntriesNumber);
+      NodesList = data.Item1?.ToList();
+      PaginationMetadata = data.Item2;
+    }
+
+    private async Task SearchEntries()
+    {
+      var data = await NodeService.GetNodesAsync(DataSearch, 1, DataEntriesNumber);
+      NodesList = data.Item1?.ToList();
+      PaginationMetadata = data.Item2;
+    }
 
     protected override async Task OnInitializedAsync()
     {
