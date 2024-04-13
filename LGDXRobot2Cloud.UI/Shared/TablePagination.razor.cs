e66ed4af7b4f01abceb6e62d693c75c6f6dc4f9c
Kai Tung Yu <sam@bristolgram.uk>
2024-04-14 00:28:24 +0100

    easy page logic

--- LGDXRobot2Cloud.UI/Shared/TablePagination.razor.cs
+++ LGDXRobot2Cloud.UI/Shared/TablePagination.razor.cs
@@ -14,10 +14,29 @@ namespace LGDXRobot2Cloud.UI.Shared
 
     [Parameter]
     public EventCallback<int> OnPageChange { get; set; }
-    
+
+    private SortedSet<int> _renderPage { get; set; } = [];
+
+    private int _lastIndex { get; set; } = 0;
+     
     private async Task ChangePage(MouseEventArgs e, int pageNum)
     {
       await OnPageChange.InvokeAsync(pageNum);
+    }
+
+    public override async Task SetParametersAsync(ParameterView parameters) 
+    {
+      if (parameters.TryGetValue<PaginationMetadata>(nameof(PaginationMetadata), out var value))
+      {
+        var pageCount = value.PageCount;
+        var currentPage = value.CurrentPage;
+        _renderPage.Clear();
+        for(int i = currentPage - 3; i <= currentPage + 3; i++)
+          if (i > 1 && i < pageCount) _renderPage.Add(i);
+        _renderPage.Add(1);
+        _renderPage.Add(pageCount);
+      }
+      await base.SetParametersAsync(parameters);
     }
   }
 }
