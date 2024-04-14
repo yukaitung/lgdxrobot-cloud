c2f143df418fd0e901271ba9f1842af3d67c8ded
Kai Tung Yu <sam@bristolgram.uk>
2024-04-14 20:17:13 +0100

    ModalSubmitDone code refactor

--- LGDXRobot2Cloud.UI/Pages/Nodes.razor.cs
+++ LGDXRobot2Cloud.UI/Pages/Nodes.razor.cs
@@ -1,39 +1,20 @@
-using LGDXRobot2Cloud.Shared.Entities;
-using LGDXRobot2Cloud.Shared.Services;
 using LGDXRobot2Cloud.UI.Helpers;
-using LGDXRobot2Cloud.UI.Services;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using LGDXRobot2Cloud.UI.Components.Nodes;
+using LGDXRobot2Cloud.UI.Shared;
 
 namespace LGDXRobot2Cloud.UI.Pages
 {
   public partial class Nodes
   {
     private int? _nodeId { get; set; } = null;
-
-    private NodeTable? NodeTable { get; set; }
-
-    // Data Edit
-    private bool _submitDone { get; set; } = false;
-    private CrudOperation _submitCrud { get; set; }
-    private int _submitDoneId { get; set; }
-    private string _submitDoneItemName { get; set; } = string.Empty;
-
+    private NodeTable? _nodeTable { get; set; }
+    private ModalSubmitDone? _modalSubmitDone { get; set; }
 
     private async Task HandleSubmitDoneOpen((int, string, CrudOperation) data)
     {
-      _submitDone = true;
-      _submitDoneId = data.Item1;
-      _submitDoneItemName = data.Item2;
-      _submitCrud = data.Item3;
       _nodeId = null;
-      await NodeTable!.Refresh(_submitCrud == CrudOperation.Delete);
-    }
-
-    private void HandleSubmitDoneClose(bool display)
-    {
-      _submitDone = display;
+      _modalSubmitDone!.Open(data.Item1, data.Item2,data.Item3);
+      await _nodeTable!.Refresh(data.Item3 == CrudOperation.Delete);
     }
 
     private void HandleItemSelect(int id)
