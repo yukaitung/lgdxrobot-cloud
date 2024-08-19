a33b5d19d1672e54dfa13d4733b9f2ded2b40bdc
Kai Tung Yu <sam@bristolgram.uk>
2024-08-19 22:01:35 +0100

    auto run script program

---
+++ LGDXRobot2Cloud.Data/Program.cs
@@ -0,0 +1,19 @@
+using LGDXRobot2Cloud.Data.DbContexts;
+using LGDXRobot2Cloud.Data.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("secrets.json", true, true);
+var connectionString = builder.Configuration["MySQLConnectionString"];
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 36));
+builder.Services.AddDbContext<LgdxContext>(
+    dbContextOptions => dbContextOptions
+        .UseMySql(connectionString, serverVersion)
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors()
+);
+builder.Services.AddHostedService<SqlScriptRunner>();
+
+var app = builder.Build();
+app.Run();
