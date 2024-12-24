ea9b19ed3def995bb663181e858a2caac050cc4a
Kai Tung Yu <sam@bristolgram.uk>
2024-12-24 18:31:37 +0000

    touch support

--- LGDXRobot2Cloud.UI/wwwroot/js/navigation-map.js
+++ LGDXRobot2Cloud.UI/wwwroot/js/navigation-map.js
@@ -1,9 +1,12 @@
 var canvas;
+var startScale;
 var two;
 var windowHeight = window.innerHeight;
 var zui;
-var startScale;
 
+/*
+ * Dotnet Functions
+ */
 function InitNavigationMap() 
 {
   // Init Two.js
@@ -29,67 +32,124 @@ function InitNavigationMap()
   mapRect.noStroke();
   two.add(mapRect);
 
-  AddZui(canvas.width, canvas.height, imageWidth, imageHeight);
+  // Add robots
+  var robot = two.makeCircle(x, y, 3);
+  robot.fill = '#FF8000';
+  robot.stroke = 'orangered';
+  two.add(robot);
+
+  // Add event listener
+  two.renderer.domElement.addEventListener('click', _internalOnRobotSelect, false);
+  two.renderer.domElement.addEventListener('touchstart', _internalOnRobotTouched, false);
+
+  // Add ZUI
+  _internalAddZui(canvas.width, canvas.height, imageWidth, imageHeight);
 
   // Disabe image smoothing
   canvas.getContext("2d").imageSmoothingEnabled = false;
+
+  // Resize event listener
+  window.addEventListener("resize", _internalOnResize);
+
+  function _internalOnRobotSelect(e)
+  {
+    var rect = robot.getBoundingClientRect();
+    var rect2 = canvas.getBoundingClientRect();
+    var x = e.clientX - (rect2.left + window.scrollX);
+    var y = e.clientY - (rect2.top + window.scrollY);
+    console.log(x, y);
+    if (x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom) {
+      // your custom function
+      console.log("I am a robot!");
+    }
+  }
+  function _internalOnRobotTouched(e)
+  {
+    var touch = e.touches[0];
+    var rect = robot.getBoundingClientRect();
+    var rect2 = canvas.getBoundingClientRect();
+    var x = touch.clientX - (rect2.left + window.scrollX);
+    var y = touch.clientY - (rect2.top + window.scrollY);
+    console.log(x, y);
+    if (x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom) {
+      // your custom function
+      console.log("I am a robot!");
+    }
+  }
+
+  function _internalOnResize(e)
+  {
+    e.preventDefault;
+
+    // Set Canvas size
+    const newWindowHeight = window.innerHeight;
+    const dy = newWindowHeight - windowHeight;
+    const div = document.getElementById("navigation-map-div");
+    two.renderer.setSize(div.clientWidth, div.clientHeight + (dy < 0 ? dy : 0));
+
+    // Set ZUI limits
+    const mapImage = document.getElementById("navigation-map-image");
+    _internalSetScale(div.clientWidth, div.clientHeight, mapImage.width, mapImage.height);
+
+    windowHeight = newWindowHeight;
+
+    // Disabe image smoothing
+    canvas.getContext("2d").imageSmoothingEnabled = false;
+  }
 }
 
+/*
+ * Page Functions
+ */
 function NavigationMapZoomIn()
 {
-  zui.zoomBy(0.2, canvas.width / 2, canvas.height / 2);
+  _internalZoom(0.2);
 }
 
 function NavigationMapZoomOut()
 {
-  zui.zoomBy(-0.2, canvas.width / 2, canvas.height / 2);
+  _internalZoom(-0.2);
 }
 
 function NavigationMapZoomReset()
 {
-  zui.zoomSet(startScale, canvas.width / 2, canvas.height / 2);
+  zui.reset();
+  _internalSetScale()
+  let canvas = document.getElementById("navigation-map-canvas");
+  zui.zoomSet(startScale, canvas.clientWidth / 2, canvas.clientHeight / 2);
 }
 
-window.addEventListener("resize", function(e)
+/*
+ * Private Functions
+ */
+function _internalZoom(scale)
 {
-  e.preventDefault;
+  zui.zoomBy(scale, canvas.clientWidth / 2, canvas.clientHeight / 2);
+}
 
-  // Set Canvas size
-  let newWindowHeight = window.innerHeight;
-  let diff = newWindowHeight - windowHeight;
-  let div = document.getElementById("navigation-map-div");
-  two.renderer.setSize(div.clientWidth, div.clientHeight + (diff < 0 ? diff : 0));
-
-  // Set ZUI limits
+function _internalSetScale()
+{
+  let canvas = document.getElementById("navigation-map-canvas");
   let mapImage = document.getElementById("navigation-map-image");
-  SetScale(div.clientWidth, div.clientHeight, mapImage.width, mapImage.height);
 
-  windowHeight = newWindowHeight;
-
-  // Disabe image smoothing
-  canvas.getContext("2d").imageSmoothingEnabled = false;
-});
-
-function SetScale(canvasWidth, canvasHeight, imageWidth, imageHeight)
-{
   // Fit map to screen
-  const canvasAspectRatio = canvasWidth / canvasHeight;
-  const imageAspectRatio = imageWidth / imageHeight;
+  const canvasAspectRatio = canvas.width / canvas.height;
+  const imageAspectRatio = mapImage.width / mapImage.height;
   if (imageAspectRatio > canvasAspectRatio) 
   {
     // Image is wider than canvas
-    startScale = canvasWidth / imageWidth;
+    startScale = canvas.clientWidth / mapImage.width;
   }
   else 
   {
     // Image is taller than canvas
-    startScale = canvasHeight / imageHeight;
+    startScale = canvas.clientHeight / mapImage.height;
   }
   zui.limits.scale.min = startScale;
   zui.limits.scale.max = 10;
 }
 
-function AddZui(canvasWidth, canvasHeight, imageWidth, imageHeight)
+function _internalAddZui()
 {
   // Init ZUI
   const scene = two.renderer.scene;
@@ -103,10 +163,10 @@ function AddZui(canvasWidth, canvasHeight, imageWidth, imageHeight)
   let mouse = new Two.Vector();
   let touches = {};
   let distance = 0;
-  let dragging = false;
 
-  SetScale(canvasWidth, canvasHeight, imageWidth, imageHeight);
-  zui.zoomSet(startScale, canvasWidth / 2, canvasHeight / 2);
+  _internalSetScale();
+  let canvas = document.getElementById("navigation-map-canvas");
+  zui.zoomSet(startScale, canvas.clientWidth / 2, canvas.clientHeight / 2);
 
   domElement.addEventListener('mousedown', mousedown, false);
   domElement.addEventListener('mousewheel', mousewheel, false);
@@ -117,7 +177,8 @@ function AddZui(canvasWidth, canvasHeight, imageWidth, imageHeight)
   domElement.addEventListener('touchend', touchend, false);
   domElement.addEventListener('touchcancel', touchend, false);
 
-  function mousedown(e) {
+  function mousedown(e) 
+  {
     mouse.x = e.clientX;
     mouse.y = e.clientY;
     var rect = scene.getBoundingClientRect();
@@ -127,30 +188,31 @@ function AddZui(canvasWidth, canvasHeight, imageWidth, imageHeight)
     window.addEventListener('mouseup', mouseup, false);
   }
 
-  function mousemove(e) {
+  function mousemove(e) 
+  {
     var dx = e.clientX - mouse.x;
     var dy = e.clientY - mouse.y;
-    if (dragging) {
-      scene.position.x += dx;
-      scene.position.y += dy;
-    } else {
-      zui.translateSurface(dx, dy);
-    }
+    zui.translateSurface(dx, dy);
     mouse.set(e.clientX, e.clientY);
   }
 
-  function mouseup(e) {
+  function mouseup(e) 
+  {
     window.removeEventListener('mousemove', mousemove, false);
     window.removeEventListener('mouseup', mouseup, false);
   }
 
-  function mousewheel(e) {
+  function mousewheel(e) 
+  {
     var dy = (e.wheelDeltaY || - e.deltaY) / 1000;
-    zui.zoomBy(dy, e.clientX, e.clientY);
+    _internalZoom(dy);
   }
 
-  function touchstart(e) {
-    switch (e.touches.length) {
+  function touchstart(e) 
+  {
+    e.preventDefault();
+    switch (e.touches.length) 
+    {
       case 2:
         pinchstart(e);
         break;
@@ -160,8 +222,11 @@ function AddZui(canvasWidth, canvasHeight, imageWidth, imageHeight)
     }
   }
 
-  function touchmove(e) {
-    switch (e.touches.length) {
+  function touchmove(e) 
+  {
+    e.preventDefault();
+    switch (e.touches.length) 
+    {
       case 2:
         pinchmove(e);
         break;
@@ -171,36 +236,44 @@ function AddZui(canvasWidth, canvasHeight, imageWidth, imageHeight)
     }
   }
 
-  function touchend(e) {
+  function touchend(e)
+  {
+    e.preventDefault();
     touches = {};
-    var touch = e.touches[ 0 ];
-    if (touch) {  // Pass through for panning after pinching
+    var touch = e.touches[0];
+    if (touch) 
+    {  
+      // Pass through for panning after pinching
       mouse.x = touch.clientX;
       mouse.y = touch.clientY;
     }
   }
 
-  function panstart(e) {
-    var touch = e.touches[ 0 ];
+  function panstart(e) 
+  {
+    var touch = e.touches[0];
     mouse.x = touch.clientX;
     mouse.y = touch.clientY;
   }
 
-  function panmove(e) {
-    var touch = e.touches[ 0 ];
+  function panmove(e) 
+  {
+    var touch = e.touches[0];
     var dx = touch.clientX - mouse.x;
     var dy = touch.clientY - mouse.y;
     zui.translateSurface(dx, dy);
     mouse.set(touch.clientX, touch.clientY);
   }
 
-  function pinchstart(e) {
-    for (var i = 0; i < e.touches.length; i++) {
-      var touch = e.touches[ i ];
+  function pinchstart(e) 
+  {
+    for (var i = 0; i < e.touches.length; i++) 
+    {
+      var touch = e.touches[i];
       touches[ touch.identifier ] = touch;
     }
-    var a = touches[ 0 ];
-    var b = touches[ 1 ];
+    var a = touches[0];
+    var b = touches[1];
     var dx = b.clientX - a.clientX;
     var dy = b.clientY - a.clientY;
     distance = Math.sqrt(dx * dx + dy * dy);
@@ -208,18 +281,23 @@ function AddZui(canvasWidth, canvasHeight, imageWidth, imageHeight)
     mouse.y = dy / 2 + a.clientY;
   }
 
-  function pinchmove(e) {
-    for (var i = 0; i < e.touches.length; i++) {
-      var touch = e.touches[ i ];
-      touches[ touch.identifier ] = touch;
+  function pinchmove(e) 
+  {
+    for (var i = 0; i < e.touches.length; i++) 
+    {
+      var touch = e.touches[i];
+      touches[i] = touch;
     }
-    var a = touches[ 0 ];
-    var b = touches[ 1 ];
+    var a = touches[0];
+    var b = touches[1];
     var dx = b.clientX - a.clientX;
     var dy = b.clientY - a.clientY;
     var d = Math.sqrt(dx * dx + dy * dy);
     var delta = d - distance;
-    zui.zoomBy(delta / 250, mouse.x, mouse.y);
+    if (delta <= 10 && delta >= -10)
+    {
+      _internalZoom(delta / 100);
+    }
     distance = d;
   }
 }
