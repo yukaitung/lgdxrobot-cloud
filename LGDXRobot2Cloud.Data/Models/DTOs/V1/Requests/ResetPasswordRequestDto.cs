38776b1c6bad4061f2b51eac71ad016f7434f65c
Kai Tung Yu <sam@bristolgram.uk>
2024-12-27 18:50:55 +0000

    record dto

--- LGDXRobot2Cloud.Data/Models/DTOs/V1/Requests/ResetPasswordRequestDto.cs
+++ LGDXRobot2Cloud.Data/Models/DTOs/V1/Requests/ResetPasswordRequestDto.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace LGDXRobot2Cloud.Data.Models.DTOs.V1.Requests;
 
-public class ResetPasswordRequestDto
+public record ResetPasswordRequestDto
 {
   [Required]
   public required string Email { get; set; }
