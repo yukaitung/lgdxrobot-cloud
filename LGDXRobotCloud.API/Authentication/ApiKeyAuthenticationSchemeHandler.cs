a4934ea065e7d2b7ba8827798422bff64528996b
Kai Tung Yu <sam@bristolgram.uk>
2025-05-06 20:06:17 +0100

    disable api key in dev

--- LGDXRobotCloud.API/Authentication/ApiKeyAuthenticationSchemeHandler.cs
+++ LGDXRobotCloud.API/Authentication/ApiKeyAuthenticationSchemeHandler.cs
@@ -10,19 +10,25 @@ public class ApiKeyAuthenticationSchemeHandler(
     IOptionsMonitor<ApiKeyAuthenticationSchemeOptions> options,
     ILoggerFactory logger,
     UrlEncoder encoder,
-    IApiKeyService apiKeyService
+    IApiKeyService apiKeyService,
+    IWebHostEnvironment webHostEnvironment
   ) : AuthenticationHandler<ApiKeyAuthenticationSchemeOptions>(options, logger, encoder)
 {
   private readonly IApiKeyService _apiKeyService = apiKeyService ?? throw new ArgumentNullException(nameof(apiKeyService));
+  private readonly IWebHostEnvironment _webHostEnvironment = webHostEnvironment ?? throw new ArgumentNullException(nameof(webHostEnvironment));
+
 
   protected override async Task<AuthenticateResult> HandleAuthenticateAsync() 
   {
-    var apiKey = Context.Request.Headers["X-API-KEY"];
-    if (string.IsNullOrWhiteSpace(apiKey) || await _apiKeyService.ValidateApiKeyAsync(apiKey!) == false) 
+    if (!_webHostEnvironment.IsDevelopment())
     {
-      return AuthenticateResult.Fail("X-API-KEY is invalid");
+      var apiKey = Context.Request.Headers["X-API-KEY"];
+      if (string.IsNullOrWhiteSpace(apiKey) || await _apiKeyService.ValidateApiKeyAsync(apiKey!) == false) 
+      {
+        return AuthenticateResult.Fail("X-API-KEY is invalid");
+      }
     }
-
+    
     var identity = new ClaimsIdentity([], Scheme.Name);
     var principal = new ClaimsPrincipal(identity);
     var ticket = new AuthenticationTicket(principal, Scheme.Name);
