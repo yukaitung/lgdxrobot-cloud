c691be1af5acdb4bb09f96d17a868f7d299505ba
Kai Tung Yu <sam@bristolgram.uk>
2025-07-20 11:49:22 +0100

    update startup script

--- LGDXRobotCloud.Data/initaliseData.sh
+++ LGDXRobotCloud.Data/initaliseData.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sleep 1s
-dotnet LGDXRobotCloud.Data.dll --initialiseData "true" --email "email@example.com" --fullName "Full Name" --userName "admin" --password "123456"
+dotnet LGDXRobotCloud.Data.dll --initialiseData "true" --email "email@example.com" --fullName "Full Name" --userName "admin" --password "123456" --seedData "true"
